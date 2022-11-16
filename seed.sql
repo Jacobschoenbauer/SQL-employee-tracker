9024b3c316030046d380b5c599c4ab9b1469d82a
Jacobschoenbauer <jhwt65@gmail.com>
2022-11-16 12:40:19 -0600

    Adde packages needed stated schema and seed

---
+++ seed.sql
@@ -0,0 +1,13 @@
+INSERT INTO department (id, name)
+VALUE (1, "Sales"),
+    (2, "Engineering"),
+    (3,"Finance" ),
+    (4, "Legal")
+
+INSERT INTO role (id, title, salary, department_id)
+VALUE (1, "",),
+    
+
+INSERT INTO employee (id, first_name, last_name, role_id, manger_id)
+VALUE (1, "Sales"),
+     
